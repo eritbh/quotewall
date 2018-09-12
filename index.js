68d2acdf21a3faab21b5323d742109b5c62b99df
George <georgej1088@gmail.com>
2018-09-11 21:37:43 -0400

    Cacheing and single quotes and 404 handling oh my

--- index.js
+++ index.js
@@ -10,6 +10,22 @@ const config = require('./config');
 let conn; // The rethinkdb connection to run queries against
 r.connect(config.rethinkdb).then(connection => conn = connection);
 
+const filecache = new Map();
+/**
+ * Helper function to read a file. In production, files are cached in memory to
+ * prevent unnecesary filesystem calls.
+ * @param {string} filename The name of the file to read.
+ * @returns {string} The contents of the file.
+ */
+function cachedRead (filename) {
+	if (config.dev) return fs.readFileSync(filename, 'utf-8');
+	const cached = filecache.get(filename);
+	if (cached) return cached;
+	const contents = fs.readFileSync(filename, 'utf-8');
+	filecache.set(filename, contents);
+	return contents;
+}
+
 /**
  * Helper function to render an ejs file and send its output in a response.
  * @param {*} res The response to send the rendered output in.
@@ -17,32 +33,35 @@ r.connect(config.rethinkdb).then(connection => conn = connection);
  * @param {*} data The data to pass to the renderer.
  */
 function render (res, filename, data) {
-	res.end(ejs.render(fs.readFileSync(filename, 'utf-8'), data));
+	res.end(ejs.render(cachedRead(filename), data));
 }
 
-const app = polka();
-
-app.get('/', (req, res) => {
-	render(res, 'pages/index.ejs', {
-		quotes: [
-			{
-				text: 'no u',
-				attribution: 'George, 2018',
-				id: 0
-			}
-		]
-	});
+const app = polka({
+	onNoMatch (req, res) {
+		res.writeHead(301, {Location: '/'});
+		res.end();
+	}
 });
 
-app.get('/random', (req, res) => {
-	res.end('TODO');
+app.get('/', async (req, res) => {
+	const quotesCursor = await r.table('quotes').run(conn);
+	const quotes = await quotesCursor.toArray();
+	render(res, 'pages/index.ejs', {quotes});
 });
 
-app.get('/quote/:id', (req, res) => {
-	res.end('TODO');
+app.get('/quote/random', async (req, res) => {
+	const quote = await r.table('quotes').sample(1).nth(0).run(conn) || {};
+	log.info(quote);
+	render(res, 'pages/single.ejs', {quote});
+});
+
+app.get('/quote/:id', async (req, res) => {
+	const quote = await r.table('quotes').get(req.params.id).run(conn) || {};
+	render(res, 'pages/single.ejs', {quote});
 });
 
 app.post('/quote/new', (req, res) => {
+	log.info(req);
 	res.end('TODO');
 });
 
